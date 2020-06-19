6d658ad3d9253934bdc5df674ac57eb5e83cabd7
Ariel <arielbs1991@gmail.com>
2020-06-19 10:01:57 -0700

    hopefully working relative time color change

--- script.js
+++ script.js
@@ -16,13 +16,93 @@
 var d = new Date();
 var startDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").append(startDate);
-textArea1=$("#9am");
-textArea2=$("#10am");
-textArea3=$("#11am");
-textArea4=$("#12pm");
-textArea5=$("#1pm");
-textArea6=$("#2pm");
-textArea7=$("#3pm");
-textArea8=$("#4pm");
-textArea9=$("#5pm");
+textArea1 = $("#09");
+textArea2 = $("#10");
+textArea3 = $("#11");
+textArea4 = $("#12");
+textArea5 = $("#13");
+textArea6 = $("#14");
+textArea7 = $("#15");
+textArea8 = $("#16");
+textArea9 = $("#17");
+
+var textDisplayBox = $('.inputField');
+textDisplayBox.placeholder = "Click here to enter event in this timeblock"; //need to put on the page
+
+var hourRow = $('.time-block');
+
+var currentHour = moment().format("HH");
+var eventHour = $('.hour');
+console.log(textArea1.attr("id"));
+console.log(currentHour);
+
+function loadPage (){
+if (currentHour > textArea1.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea1.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea1.attr("id")) {
+    hourRow.css("background-color", "blue");
+}};
+if (currentHour > textArea2.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea2.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea2.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea3.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea3.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea3.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea4.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea4.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea4.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea5.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea5.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea5.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea6.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea6.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea6.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea7.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea7.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea7.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea8.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea8.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea8.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+if (currentHour > textArea9.attr("id")) {
+    hourRow.css("background-color", "gray");
+} else if (startDate === textArea9.attr("id")) {
+    hourRow.css("background-color", "green");
+} else if (startDate < textArea9.attr("id")) {
+    hourRow.css("background-color", "blue");
+};
+loadPage();
+//something is wrong with the values of my time, come back to it with help
+
+
+
 
