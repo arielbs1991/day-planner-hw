1cb15ef6e9300772402530ad1b0a79ca9dc68945
Ariel <arielbs1991@gmail.com>
2020-06-19 21:48:26 -0700

    info displays on page load now, just need to finish the readme!

--- script.js
+++ script.js
@@ -130,63 +130,116 @@ loadPage();
 //     localStorage.setItem("data", input);
 // });
 // var input1 = ($("textarea")[i].value);
+// var dataAray = "";
 
 $("#9amBtn").on("click", function () {
     var input1 = $("textarea")[0].value;
-    localStorage.setItem("data1", input1);
+    localStorage.setItem("data1", JSON.stringify(input1));
+    // dataAray.push("data1");
     console.log(input1);
 });
 $("#10amBtn").on("click", function () {
     var input2 = $("textarea")[1].value;
-    localStorage.setItem("data2", input2);
+    localStorage.setItem("data2", JSON.stringify(input2));
+    // dataAray.push("data2");
     console.log(input2);
 });
 $("#11amBtn").on("click", function () {
     var input3 = $("textarea")[2].value;
-    localStorage.setItem("data3", input3);
+    localStorage.setItem("data3", JSON.stringify(input3));
     console.log(input3);
 });
 $("#12pmBtn").on("click", function () {
     var input4 = $("textarea")[3].value;
-    localStorage.setItem("data4", input4);
+    localStorage.setItem("data4", JSON.stringify(input4));
     console.log(input4);
 });
 $("#1pmBtn").on("click", function () {
     var input5 = $("textarea")[4].value;
-    localStorage.setItem("data5", input5);
+    localStorage.setItem("data5", JSON.stringify(input5));
     console.log(input5);
 });
 $("#2pmBtn").on("click", function () {
     var input6 = $("textarea")[5].value;
-    localStorage.setItem("data6", input6);
+    localStorage.setItem("data6", JSON.stringify(input6));
     console.log(input6);
 });
 $("#3pmBtn").on("click", function () {
     var input7 = $("textarea")[6].value;
-    localStorage.setItem("data7", input7);
+    localStorage.setItem("data7", JSON.stringify(input7));
     console.log(input7);
 });
 $("#4pmBtn").on("click", function () {
     var input8 = $("textarea")[7].value;
-    localStorage.setItem("data8", input8);
+    localStorage.setItem("data8", JSON.stringify(input8));
     console.log(input8);
 });
 $("#5pmBtn").on("click", function () {
     var input9 = $("textarea")[8].value;
-    localStorage.setItem("data9", input9);
+    localStorage.setItem("data9", JSON.stringify(input9));
     console.log(input9);
 });
 $("#6pmBtn").on("click", function () {
     var input10 = $("textarea")[9].value;
-    localStorage.setItem("data10", input10);
+    localStorage.setItem("data10", JSON.stringify(input10));
     console.log(input10);
 });
 
-// var inputText1 = JSON.parse(localStorage.getItem("data1"));
-var data = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10];
+// console.log(dataAray);
 
+var inputText1 = JSON.parse(localStorage.getItem("data1"));
+var inputText2 = JSON.parse(localStorage.getItem("data2"));
+var inputText3 = JSON.parse(localStorage.getItem("data3"));
+var inputText4 = JSON.parse(localStorage.getItem("data4"));
+var inputText5 = JSON.parse(localStorage.getItem("data5"));
+var inputText6 = JSON.parse(localStorage.getItem("data6"));
+var inputText7 = JSON.parse(localStorage.getItem("data7"));
+var inputText8 = JSON.parse(localStorage.getItem("data8"));
+var inputText9 = JSON.parse(localStorage.getItem("data9"));
+var inputText10 = JSON.parse(localStorage.getItem("data10"));
 
+function load() {
+    $("#09").text(localStorage.getItem("data1"));
+    $("#10").text(localStorage.getItem("data2"));
+    $("#11").text(localStorage.getItem("data3"));
+    $("#12").text(localStorage.getItem("data4"));
+    $("#13").text(localStorage.getItem("data5"));
+    $("#14").text(localStorage.getItem("data6"));
+    $("#15").text(localStorage.getItem("data7"));
+    $("#16").text(localStorage.getItem("data8"));
+    $("#17").text(localStorage.getItem("data9"));
+    $("#18").text(localStorage.getItem("data10"));
+}
+load();
+finalArray = [];
 
+finalArray.push(inputText1, inputText2, inputText3, inputText4, inputText5, inputText6, inputText7, inputText8, inputText9, inputText10);
 
+console.log(finalArray);
 
+var inputArray = JSON.parse(localStorage.getItem(finalArray));
 
+// $(window).append(localStorage.getItem(finalArray));
+// function loadData() {
+//     for(i=0; i < finalArray.length; i++) {
+//     $('#calendar').append('<textarea>' + finalArray[i] + '</textarea>');
+// }}
+// loadData();
+// var data = [
+//     data.push("data1"),
+//     push(data2),
+//     push(data3),
+//     push(data4),
+//     push(data5),
+//     push(data6),
+//     push(data7),
+//     push(data8),
+//     push(data9),
+//     push(data10)
+// ];
+
+// console.log(data);
+
+// var data = data.push(this);
+
+// JSON.parse(localStorage.getItem('data'));
